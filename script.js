619a5540e10fa6e246d40a8feb515ed2ca0927a7
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-10 22:48:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let btn = document.getElementById('swap');
 let div = document.getElementById('app')
 btn.addEventListener('click',()=>{
-	div.classList.add('night');
-	btn.classList.add('button_night';)
+	div.classList.toggle('night')
+	btn.classList.toggle('button_night')
 })
